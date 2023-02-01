6cf03828daeb3d47d550a291c59a70c87dda4075
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2023-02-01 12:15:32 +0100

    Add visual effect Button Cart

--- src/components/Cart/HeaderCartButton.js
+++ src/components/Cart/HeaderCartButton.js
@@ -1,14 +1,33 @@
 import styles from './HeaderCartButton.module.css'
 import CartIcon from './CartIcon'
 import CartContext from '../../store/cart-context'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 function HeaderCartButton(props) {
   const ctx = useContext(CartContext)
   const numberOfCartItems = ctx.items.length
 
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
+
+  useEffect(() => {
+    if (numberOfCartItems === 0) {
+      return
+    }
+    setBtnIsHighlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [ctx.items])
+
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
