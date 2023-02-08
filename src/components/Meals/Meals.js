ff52be633de491752259b530c2dd7fc14b633f2d
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2023-02-08 19:12:30 +0100

    Fetch meals from backend

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,14 +1,40 @@
 import styles from './Meals.module.css'
-import DUMMY_MEALS from '../../dummy-meals'
 import Card from '../UI/Card'
 import MealItem from './MealItem'
+import useHttp from '../../hooks/use-http'
+import { useEffect, useState } from 'react'
+
+const BASE_URL =
+  'https://orderfoodappreact-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
 
 const Meals = () => {
+  const [meals, setMeals] = useState([])
+  const { isLoading, error, sendRequest: getMeals } = useHttp()
+
+  useEffect(() => {
+    const transformMeals = (meals) => {
+      const loadedMeals = []
+      for (const key in meals) {
+        loadedMeals.push({
+          id: key,
+          name: meals[key].name,
+          description: meals[key].description,
+          price: meals[key].price,
+        })
+      }
+      setMeals(loadedMeals)
+    }
+
+    getMeals({ url: BASE_URL }, transformMeals)
+  }, [getMeals])
+
   return (
     <section className={styles.meals}>
       <Card>
-        {DUMMY_MEALS.map((meal) => (
-          <MealItem item={meal} key={meal.id} />
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {meals.map((meal) => (
+            <MealItem item={meal} key={meal.id} />
         ))}
       </Card>
     </section>
